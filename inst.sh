8ab6cb69b41795da7486f7618ab84834cc77d2a7
caotu2k5 <71805958+caotu2k5@users.noreply.github.com>
2024-08-25 09:55:36 +0700

    Update inst.sh

--- inst.sh
+++ inst.sh
@@ -2,7 +2,7 @@ clear
 echo "Đang cài đặt"
 apt update && apt upgrade -y
 clear
-cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && apt install wget -y && wget https://github.com/caotu2k5/test/raw/main/v111.zip && unzip v111.zip && chmod a+x neko && chmod a+x tun && chmod a+x psiphon-tunnel-core && chmod a+x qexit && chmod a+x wl && chmod a+x ver && chmod a+x v111man
+cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && apt install wget -y && wget https://github.com/caotu2k5/test/raw/main/v111.zip && unzip v111.zip && chmod +x neko && chmod +x tun && chmod +x psiphon-tunnel-core && chmod +x qexit && chmod +x wl && chmod +x ver && chmod +x v111man
 clear
 cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/etc/ && apt install wget -y && wget https://github.com/caotu2k5/test/raw/main/t1 && mv t1 motd
 clear
