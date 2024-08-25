839abe64d30388f32732c85848f0e8582eddfaa2
caotu2k5 <71805958+caotu2k5@users.noreply.github.com>
2024-08-25 09:52:42 +0700

    Update inst.sh

--- inst.sh
+++ inst.sh
@@ -1,12 +1,12 @@
 clear
 echo "Đang cài đặt"
-apt-get update && apt-get upgrade -y
+apt update && apt upgrade -y
 clear
-cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && apt-get install wget -y && wget https://github.com/caotu2k5/test/raw/main/v111.zip && unzip v111.zip && chmod a+x neko && chmod a+x tun && chmod a+x psiphon-tunnel-core && chmod a+x qexit && chmod a+x wl && chmod a+x ver && chmod a+x v111man
+cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && apt install wget -y && wget https://github.com/caotu2k5/test/raw/main/v111.zip && unzip v111.zip && chmod a+x neko && chmod a+x tun && chmod a+x psiphon-tunnel-core && chmod a+x qexit && chmod a+x wl && chmod a+x ver && chmod a+x v111man
 clear
-cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/etc/ && apt-get install wget -y && wget https://github.com/caotu2k5/test/raw/main/t1 && mv t1 motd
+cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/etc/ && apt install wget -y && wget https://github.com/caotu2k5/test/raw/main/t1 && mv t1 motd
 clear
-cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && apt-get install wget -y && wget https://github.com/caotu2k5/test/raw/main/l1 && chmod +x * && mv l1 login &&
+cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && apt install wget -y && wget https://github.com/caotu2k5/test/raw/main/l1 && chmod +x * && mv l1 login &&
 clear
 echo "Đã cài đặt thành công"
 login
